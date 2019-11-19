f59711e984d760540e2a212bbc8a824a8d883b23
Nicky <nickyliu91@gmail.com>
2019-11-18 23:35:25 -0500

    added state and on click to begin

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default class HomePage extends React.Component {
+
+  state = {
+    show: 'home'
+  }
+
   render() {
-    return(
-      <div id='homePage'>
+    if (this.state.show == 'home') {
+      return(
+        <div id='homePage'>
         <img src="images/cover.jpg" />
         <p>Augmented Reality</p>
-        <p>Begin</p>
-      </div>
-    )
+        <p onClick={() => {console.log("hi")}}>Begin</p>
+        </div>
+      )
+    }
   }
 }
