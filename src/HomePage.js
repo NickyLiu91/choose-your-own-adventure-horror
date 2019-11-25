85001190b39d977da3d5084963b8506bb49f2fb9
Nicky <nickyliu91@gmail.com>
2019-11-24 23:46:35 -0500

    restructured story to array and remove area array

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,24 +4,43 @@ let areaArray = [
   "cover", "bedroom", "gear", "bedroom"
 ]
 
-const story = {
-    "cover": ["Augmented Reality"]
+// const story = {
+//     "cover": ["Augmented Reality"]
+//   ,
+//     "bedroom": [
+//     "You open your eyes and look around. You are in your nerded out room with your bed which looked like a spaceship and sci-fi figures everywhere. Star Wars, Star Trek, Aliens, every nerdy franchise you can think of.",
+//     "But all of that pales in comparison to what arrives today...",
+//     "Alien Invasion!"
+//   ],
+//   "gear": [
+//     "Alien Invasion was the hottest and most anticipated release of the year.",
+//     "It was an augmented reality game that cmae with a headset and 'gun', and would augment your vision to show aliens in random places.",
+//     "Players had to shoot and kill these aliens, and the player who killed the most aliens would receive an invitiation to meet the developers themselves at Area 51."
+//   ],
+//   "bedroom": [
+//     "Seeing what's inside Area 51 is every sci-fi nerd's dream!",
+//     "'I will definitely being the greatest alien slayer!' you declare loudly as you hold up the gaming equipment."
+//   ]
+// }
+
+const story = [
+    {"cover": ["Augmented Reality"]}
   ,
-    "bedroom": [
+    {"bedroom": [
     "You open your eyes and look around. You are in your nerded out room with your bed which looked like a spaceship and sci-fi figures everywhere. Star Wars, Star Trek, Aliens, every nerdy franchise you can think of.",
     "But all of that pales in comparison to what arrives today...",
     "Alien Invasion!"
-  ],
-  "gear": [
+  ]},
+  {"gear": [
     "Alien Invasion was the hottest and most anticipated release of the year.",
     "It was an augmented reality game that cmae with a headset and 'gun', and would augment your vision to show aliens in random places.",
     "Players had to shoot and kill these aliens, and the player who killed the most aliens would receive an invitiation to meet the developers themselves at Area 51."
-  ],
-  "bedroom": [
+  ]},
+  {"bedroom": [
     "Seeing what's inside Area 51 is every sci-fi nerd's dream!",
     "'I will definitely being the greatest alien slayer!' you declare loudly as you hold up the gaming equipment."
-  ]
-}
+  ]}
+]
 
 export default class HomePage extends React.Component {
 
@@ -31,7 +50,20 @@ export default class HomePage extends React.Component {
   }
 
   next = () => {
-    if (this.state.text == story[areaArray[this.state.area]].length - 1) {
+    // if (this.state.text == story[areaArray[this.state.area]].length - 1) {
+    //   this.setState({
+    //     area: this.state.area + 1,
+    //     text: 0
+    //   })
+    // } else {
+    //   this.setState({
+    //     text: this.state.text + 1
+    //   })
+    // }
+    console.log(this.state.text)
+    console.log(Object.values(story[this.state.area]))
+    // console.log(Object.values(story[this.state.area])[0])
+    if (Object.values(story[this.state.area])[this.state.text] == Object.values(story[this.state.area]).length - 1) {
       this.setState({
         area: this.state.area + 1,
         text: 0
@@ -48,15 +80,15 @@ export default class HomePage extends React.Component {
       return(
         <div id='homePage'>
           <img src="images/cover.jpg" />
-          <p>{story[areaArray[this.state.area]][this.state.text]}</p>
+          <p>{Object.values(story[this.state.area])[this.state.text]}</p>
           <p onClick={(event) => {this.next(event)}}>Begin</p>
         </div>
       )
     } else {
       return(
         <div id='homePage'>
-          <img src={"images/" + areaArray[this.state.area] + ".jpg"} />
-          <p>{story[areaArray[this.state.area]][this.state.text]}</p>
+          <img src={"images/" + Object.keys(story[this.state.area][0]) + ".jpg"} />
+          <p>{Object.values(story[this.state.area])[this.state.text][0]}</p>
           <p onClick={(event) => {this.next(event)}}>Next</p>
         </div>
       )
